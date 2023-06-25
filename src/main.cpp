d56f3f5025ee5c6b3ca7f0e617263b6c4e53dd99
Dima <ivanowdima1999@gmail.com>
2023-06-25 14:21:33 +0200

    OpenGL and glfw initialization

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,53 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+//
+//// для выбора основной видеокарты
+//extern "C" {
+//    _declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+//}
+
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode) 
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
+   
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
-    }
+    }  
+
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -29,7 +61,9 @@ int main(void)
         return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
 
     glClearColor(0.5, 1, 0.7, 1);
